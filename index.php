8451824887ae6a304e3493277daacd21d72eef1f
“Alaa4eg-h” <Alaa4eg@outlook.com>
2021-12-16 13:38:06 +0200

    first Structure

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <!-- BOOTSTRAP CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-rtl.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>تدويناتى</title>
+</head>
+
+<body>
+
+
+
+
+    <div class="btn btn-danger">إضغط هنا</div>
+
+
+
+
+
+    <!-- JQUERY  -->
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <!-- BOOSTRAP JS -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
