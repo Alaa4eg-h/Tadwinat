622e2ffb4c1ed9adfe0a33cdb72fd5ce5152fac8
“Alaa4eg-h” <Alaa4eg@outlook.com>
2021-12-19 14:40:38 +0200

    Create the Main Page

--- index.php
+++ index.php
@@ -18,17 +18,191 @@
 
 <body>
 
+    <!-- START NAV -->
+    <nav class="navbar bg-light navbar-light navbar-expand-sm">
+        <div class="container">
+            <a href="#" class="navbar-brand">تدويناتى</a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">عن المدونة</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">شروحات</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">منوعات</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">تواصل معنا</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- END NAV -->
+
+    <!-- START CONTENT -->
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="post">
+                        <div class="post-image">
+                            <img src="images/img-1.png" alt="img-1">
+                        </div>
+                        <div class="post-title">
+                            <h4>هذا النص هو مثال لنص يمكن إستبداله</h4>
+                        </div>
+                        <div class="post-details">
+                            <p class="post-info">
+                                <span><i class="fas fa-user"></i> علاء توفيق</span>
+                                <span><i class="far fa-calendar"></i> 19-12-2021</span>
+                                <span><i class="fas fa-tag"></i> بلوجر</span>
+                            </p>
+                            <p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص
+                                العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد
+                                الحروف التى يولدها التطبيق.
+                                إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما
+                                تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد النص العربى مفيد لمصممي المواقع على
+                                وجه الخصوص، حيث يحتاج العميل فى كثير من الأحيان أن يطلع على صورة حقيقية لتصميم الموقع.
+                            </p>
+                            <button class="btn btn-custom">إقرأ المزيد</button>
+                        </div>
+                    </div>
+                    <div class="post">
+                        <div class="post-image">
+                            <img src="images/img-2.png" alt="img-2">
+                        </div>
+                        <div class="post-title">
+                            <h4>هذا النص هو مثال لنص يمكن إستبداله</h4>
+                        </div>
+                        <div class="post-details">
+                            <p class="post-info">
+                                <span><i class="fas fa-user"></i> علاء توفيق</span>
+                                <span><i class="far fa-calendar"></i> 19-12-2021</span>
+                                <span><i class="fas fa-tag"></i> بلوجر</span>
+                            </p>
+                            <p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص
+                                العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد
+                                الحروف التى يولدها التطبيق.
+                                إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما
+                                تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد النص العربى مفيد لمصممي المواقع على
+                                وجه الخصوص، حيث يحتاج العميل فى كثير من الأحيان أن يطلع على صورة حقيقية لتصميم الموقع.
+                            </p>
+                            <button class="btn btn-custom">إقرأ المزيد</button>
+                        </div>
+                    </div>
+                    <div class="post">
+                        <div class="post-image">
+                            <img src="images/img-3.png" alt="img-3">
+                        </div>
+                        <div class="post-title">
+                            <h4>هذا النص هو مثال لنص يمكن إستبداله</h4>
+                        </div>
+                        <div class="post-details">
+                            <p class="post-info">
+                                <span><i class="fas fa-user"></i> علاء توفيق</span>
+                                <span><i class="far fa-calendar"></i> 19-12-2021</span>
+                                <span><i class="fas fa-tag"></i> بلوجر</span>
+                            </p>
+                            <p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص
+                                العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد
+                                الحروف التى يولدها التطبيق.
+                                إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما
+                                تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد النص العربى مفيد لمصممي المواقع على
+                                وجه الخصوص، حيث يحتاج العميل فى كثير من الأحيان أن يطلع على صورة حقيقية لتصميم الموقع.
+                            </p>
+                            <button class="btn btn-custom">إقرأ المزيد</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <!-- START CATEGORIES -->
+                    <div class="categories">
+                        <h4>التصنيفات</h4>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <span><i class="fas fa-tag"></i></span>
+                                    <span>بلوجر</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span><i class="fas fa-tag"></i></span>
+                                    <span>يوتيوب</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span><i class="fas fa-tag"></i></span>
+                                    <span>دورات</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span><i class="fas fa-tag"></i></span>
+                                    <span>بلوجر</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span><i class="fas fa-tag"></i></span>
+                                    <span>أندرويد</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- END CATEGORIES  -->
+                    <!-- START LATEST POSTS -->
+                    <div class="last-post">
+                        <h4>أحدث المنشورات</h4>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <span class="span-image"><img src="images/img-1.png" alt="img-1"></span>
+                                    <span> هذا النص هو مثال لنص يمكن إستبداله فى نفس المساحة</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="span-image"><img src="images/img-2.png" alt="img-2"></span>
+                                    <span> هذا النص هو مثال لنص يمكن إستبداله فى نفس المساحة</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="span-image"><img src="images/img-3.png" alt="img-3"></span>
+                                    <span> هذا النص هو مثال لنص يمكن إستبداله فى نفس المساحة</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- END LATEST POSTS -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END CONTENT -->
 
 
-
-    <div class="btn btn-danger">إضغط هنا</div>
-
-
+    <!-- START FOOTER -->
+    <footer>
+        <p>جميع الحقوق محفوظة &copy; 2022</p>
+    </footer>
+    <!-- END FOOTER -->
 
 
 
     <!-- JQUERY  -->
     <script src="js/jquery-3.6.0.min.js"></script>
+    <!-- FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/44c61da6d8.js" crossorigin="anonymous"></script>
     <!-- BOOSTRAP JS -->
     <script src="js/bootstrap.min.js"></script>
 </body>
